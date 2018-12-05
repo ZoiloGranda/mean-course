89628a31e22160d1043ddd204eff6fb90728ea90
Zoilo Granda <zgranda@gmail.com>
2018-12-05 10:47:52 -0400

    9. Adding a Toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatInputModule, MatCardModule, MatButtonModule } from '@angular/material';
-
-import { AppComponent } from './app.component';
-import { PostCreateComponent } from './posts/post-create/post-create.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    PostCreateComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    BrowserAnimationsModule,
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent] //the main root component, every other Component
-  //will be inside of this one
-})
-export class AppModule { }
+import { MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule } from '@angular/material';
+  
+  import { AppComponent } from './app.component';
+  import { PostCreateComponent } from './posts/post-create/post-create.component';
+  import { HeaderComponent } from './header/header/header.component';
+  
+  @NgModule({
+    declarations: [
+      AppComponent,
+      PostCreateComponent,
+      HeaderComponent
+    ],
+    imports: [
+      BrowserModule,
+      FormsModule,
+      BrowserAnimationsModule,
+      MatInputModule,
+      MatCardModule,
+      MatButtonModule,
+      MatToolbarModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent] //the main root component, every other Component
+    //will be inside of this one
+  })
+  export class AppModule { }
