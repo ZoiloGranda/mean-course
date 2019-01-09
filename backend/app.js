c9770e2c275464d6a3382279d27751ceb77ee1ba
Zoilo Granda <zgranda@gmail.com>
2019-01-09 08:58:35 -0400

    7. Fetching Initial Posts

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next)=>{
-  console.log('first');
-  next()
-})
-
-app.use((req, res, next)=>{
-  res.send('from express')
+app.use('/api/posts',(req, res, next)=>{
+  const posts =[
+    {id: 'asdad132', title:'first post', content:'post content'},
+    {id: 'fk28ehsd', title:'second post', content:'post content 2'}
+  ]
+  res.status(200).json({
+    message:'Posts fetched correctly',
+    posts:posts
+  })
 })
 
 module.exports = app;
