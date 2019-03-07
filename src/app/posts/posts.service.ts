480c4e161f138d1eeacc3f935f9d7582a9303f7f
Zoilo Granda <zgranda@gmail.com>
2019-03-07 10:32:27 -0400

    8. Updating Posts on the Server

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -35,8 +35,7 @@ export class PostsService {
   }
   
   getPost(id: string){
-    //search all the loaded posts, and return the one that matches the id
-    return {...this.posts.find(p=> p.id === id)}
+    return this.http.get<{_id:string, title:string, content:string}>('http://localhost:3000/api/posts/'+id)
   }
   
   addPost(title:string, content:string){
@@ -55,7 +54,14 @@ export class PostsService {
   updatePost(id: string, title: string, content:string){
     const post: Post ={id: id, title: title, content:content };
     this.http.put('http://localhost:3000/api/posts/' + id, post)
-    .subscribe(response => console.log(response));
+    .subscribe(response => {
+      console.log(response);  
+      const updatedPosts = [...this.posts];
+      const oldPostIndex = updatedPosts.findIndex(p => p.id=== post.id);
+      updatedPosts[oldPostIndex] = post;
+      this.posts = updatedPosts;
+      this.postsUpdated.next([...this.posts])
+    })
   }
   
   deletePost(postId: string){
