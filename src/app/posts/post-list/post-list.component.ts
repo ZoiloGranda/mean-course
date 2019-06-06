725b68b9f3b3e6a0b01c2c71444dc159af8ce570
Zoilo Granda <zgranda@gmail.com>
2019-06-06 08:44:05 -0400

    4. Connecting the Angular Paginator to the Backend

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -19,6 +19,7 @@ export class PostListComponent implements OnInit, OnDestroy {
   isLoading = false;
   totalPosts = 10;
   postsPerPage = 2;
+  currentPage = 1;
   pageSizeOptions = [1,2,5,10];
   private postsSub : Subscription;
   constructor(public postsService:PostsService) {
@@ -26,7 +27,7 @@ export class PostListComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.isLoading = true;
-    this.postsService.getPosts();
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
     //the first parameter that subscribe receives, is the the callback function when a new
     // event is emited
     this.postsSub = this.postsService.getPostsUpdateListener().subscribe((posts:Post[])=>{
@@ -36,7 +37,9 @@ export class PostListComponent implements OnInit, OnDestroy {
   }
   
   onChangedPage(pageData: PageEvent){
-    console.log('asdad')
+    this.currentPage = pageData.pageIndex + 1;
+    this.postsPerPage = pageData.pageSize
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
   }
   
   onDelete(postId:string){
