fb374494bc7a8d212b2dc1acb79659a450b31dcc
Zoilo Granda <zgranda@gmail.com>
2019-07-10 09:07:08 -0400

    5. Creating the User Model

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const postsRoutes = require('./routes/posts');
+const userRoutes = require('./routes/user');
 const app = express();
 
 mongoose.connect('mongodb+srv://Zoilo:8EbEIeZlHRy4eWC7@cluster0-pfpox.mongodb.net/node-angular?retryWrites=true')
@@ -24,6 +25,7 @@ app.use((req,res,next)=>{
   next()  
 })
 
-app.use('/api/posts', postsRoutes)
+app.use('/api/posts', postsRoutes);
+app.use('/api/user', userRoutes);
 
 module.exports = app;
