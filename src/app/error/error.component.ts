04aef3e5858f95131d8842f0eb28b21846bcb6e8
Zoilo Granda <zgranda@gmail.com>
2019-12-03 08:59:07 -0400

    5. Adding an Error Dialog

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
  selector: 'app-error',
  templateUrl: './error.component.html',
  styleUrls: ['./error.component.css']
 })
+
 export class ErrorComponent implements OnInit {
- message = 'An unkown error ocurred'
- constructor() { }
+ constructor(@Inject(MAT_DIALOG_DATA) public data: {message:string}) { }
  
  ngOnInit() {
  }
